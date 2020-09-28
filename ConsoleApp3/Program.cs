a50d557d755ba33d024bb5eee7fc0d7a18566116
danny dan <oeffo9@gmail.com>
2020-09-28 16:39:41 -0600

    <added solution for dectecting cycles in singly linked lists>

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 namespace ConsoleApp3
 {
     class Program
     {
         static void Main()
         {
-            int[] pee = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 5, 6, 8, 9, 9, 7, 6, 5, 3, 9,1,1,1,1,1,2,3,5, 123,12 };
+            int[] pee = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 5, 6, 8, 9, 9, 7, 6, 5, 3, 9, 1, 1, 1, 1, 1, 2, 3, 5, 123, 12 };
             Array.Sort(pee);
             int maxCount = 0;
             int value = 0;
@@ -19,7 +20,7 @@ namespace ConsoleApp3
 
                 }
                 int currentCount= j-i;
-                if (currentCount > maxCount)
+                if (currentCount >= maxCount)
                 {
                     maxCount = currentCount;
                     value = pee[i];
@@ -33,8 +34,48 @@ namespace ConsoleApp3
             //{
             //    Console.WriteLine(p);
             //}
+
         }
 
         
+
+    }
+    
+
+  public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+ 
+    public class Solution
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+            var nodeCache = new List<ListNode>();
+            while (true)
+            {
+                if (nodeCache.Contains(head.next))
+                {
+                    return true;
+                }
+                else if (head.next == null)
+                {
+                    return false;
+                }
+
+                nodeCache.Add(head);
+                head = head.next;
+
+            }
+
+        }
     }
 }
