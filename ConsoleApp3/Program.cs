9567c348e11c187e901782121f7bd9efbd961f93
danny dan <oeffo9@gmail.com>
2020-10-02 16:37:20 -0600

    added and fixed the fibonacci problem twice. did the set bit problem. fixed the number swap problem(all with steve)

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -14,14 +14,14 @@ namespace ConsoleApp3
             int maxCount = 0;
             int value = 0;
 
-            for (int i = 0; i < pee.Length - 1; i++ )
+            for (int i = 0; i < pee.Length - 1; i++)
             {
                 int j = i;
-                for ( ; j < pee.Length && pee[i] == pee[j]; j++)
+                for (; j < pee.Length && pee[i] == pee[j]; j++)
                 {
 
                 }
-                int currentCount= j-i;
+                int currentCount = j - i;
                 if (currentCount >= maxCount)
                 {
                     maxCount = currentCount;
@@ -29,33 +29,8 @@ namespace ConsoleApp3
                 }
             }
             Console.WriteLine($"{maxCount}, {value}");
-
-            Poggers.Epicness(6);
-            //Console.WriteLine(string.Join(",", pee));
-
-            //foreach (int p in pee)
-            //{
-            //    Console.WriteLine(p);
-            //}
-
+            Console.WriteLine(Fib2(20));
         }
-
-        
-
-    }
-
-    
-  public class ListNode {
-      public int val;
-      public ListNode next;
-      public ListNode(int x) {
-          val = x;
-          next = null;
-      }
-  }
- 
-    class Solution
-    {
         public bool HasCycle(ListNode head)
         {
             if (head == null)
@@ -80,26 +55,13 @@ namespace ConsoleApp3
             }
 
         }
-    }
-    class Poopoo
-    {
-        static void Pie(LinkedList<string> pee, LinkedList<string> s)
-        {
-            foreach (var n in s)
-            {
-                pee.AddLast(n);
-            }
-        }
-    }
-    class Question4
-    {
         static bool Apple(string a, string b)
         {
             char[] A = a.ToCharArray();
             Array.Sort(A);
             char[] B = b.ToCharArray();
             Array.Sort(B);
-             for (int i = 0; i < A.Length - 1; i++ )
+            for (int i = 0; i < A.Length - 1; i++)
             {
                 if (B[i] == A[i])
                 {
@@ -111,42 +73,48 @@ namespace ConsoleApp3
                 }
             }
             return true;
-
-
-
         }
-    }
-    /*
-    class Niff
-    {
-        static void Q5()
+
+        static void Pie(LinkedList<string> pee, LinkedList<string> s)
         {
-            Console.WriteLine("First Number x=_");
-            string x = Console.ReadLine();
-            Int32.Parse(x);
-            Console.WriteLine("second Number y=_");
-            string y = Console.ReadLine();
-            Int32.TryParse(y);
-            // x=3 y=2
-            y = x + y;
-            x = y - x;
-            y = y - x;
+            foreach (var n in s)
+            {
+                pee.AddLast(n);
+            }
         }
-    }*/
-    class Hasio
-    {
+
+        static void NumSwap()
+        {
+            int ReadInt(string prompt)
+            {
+                
+                int x;
+                do
+                {
+                    Console.WriteLine(prompt);
+                } while (!int.TryParse(Console.ReadLine(), out x));
+                return x;
+            }
+            var x = ReadInt("First Number x=_");
+            var y = ReadInt("Second Number y=_");
+            y = x + y;
+           x = y - x;
+           y = y - x;
+            Console.WriteLine($"{x} {y}");
+         }
+  
         static void Q6()
         {
             //given sum and array find all pairs that equal the sum
             int[] pee = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 5, 6, 8, 9, 9, 7, 6, 5, 3, 9, 1, 1, 1, 1, 1, 2, 3, 5, 123, 12, -2, 4, -3 };
             int sum = 4;
-            
+
 
             foreach (int a in pee)
             {
-                 for (int i = 1; i < pee.Length - 1; i++ )
+                for (int i = 1; i < pee.Length - 1; i++)
                 {
-                    if (a+i == sum)
+                    if (a + i == sum)
                     {
                         string pairs = "" + a + "+" + i;
                     }
@@ -156,27 +124,58 @@ namespace ConsoleApp3
 
 
         }
-    }
-    public class Poggers
-    {
-        public static int Epicness(int n)
+        //Write a function to count a total number of set bits in a 32 bit Integer? (Solution)
+        public static uint SetBitCount(uint i)
         {
-            int[] poo = new int[n - 1];
-            poo[0] = 0;
-            poo[1] = 1;
+            uint poohead = 0;
+            while(i != 0)
+            {
+                poohead += i & 1;
+                i >>= 1;
+            }
+            return poohead;
+        }
+        public static int Fibonacci(int n)
+        {
+            //im sorry
+            int[] num = new int[n + 1];
+            num[0] = 0;
+            num[1] = 1;
             if (n <= 1)
             {
-            return n;
+                return n;
             }
             for (int i = 2; i <= n; i++)
             {
-                int a = poo[i-2] + poo[i - 1];
-                poo[i] = a;
-                
+                int a = num[i - 2] + num[i - 1];
+                num[i] = a;
+
             }
-            return poo[n];
+            return num[n];
+        }
+        public static int Fib2(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            return Fib2(n - 1) + Fib2(n - 2);
         }
     }
 
-
 }
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+        
+
+ 
+
+       
+
